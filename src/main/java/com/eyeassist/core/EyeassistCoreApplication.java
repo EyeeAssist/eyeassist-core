33dfeae3c80167cf06fff42b46ca61e7809d845e
HR-2019 <U201722123@upc.edu.pe>
2023-08-22 00:21:49 -0500

    Proyecto base

---
+++ src/main/java/com/eyeassist/core/EyeassistCoreApplication.java
@@ -0,0 +1,13 @@
+package com.eyeassist.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyeassistCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyeassistCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eyeassist/core/EyeassistCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyeassist.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyeassistCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
